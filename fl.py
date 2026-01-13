14a5947c290389193d21c3dfe04fc1ab609de776
Ashutosh Ranjan <144982118+Ashutosh-Ranjan310106@users.noreply.github.com>
2026-01-13 23:13:37 +0530

    sanitized the repo

--- fl.py
+++
@@ -1,9 +0,0 @@
-import flwr as fl
-
-if __name__ == "__main__":
-    # Start Flower server with FedAvg
-    fl.server.start_server(
-        server_address="localhost:8080",
-        config=fl.server.ServerConfig(num_rounds=3)
-    )
-
